2d825f71f6e86a69b9a795e78784632eb1c8831c
Mehul Shetty <mehul@MacBook-18-Pro-Max-Deluxe-Edition.local>
2025-02-06 11:46:03 -0800

    preprocessed the dataset

---
+++ part_one/clean.py
@@ -0,0 +1,13 @@
+import pandas as pd 
+
+df = pd.read_csv('../dataset/nyc_dataset.csv')
+
+# Remove Empty Entries
+df.dropna(subset=['@id', '@lat', '@lon'], inplace=True)
+
+# Check if IDs are all numeric
+df['@id'] = df['@id'].astype(str)
+df = df[df['@id'].str.isdigit()]
+
+# Drop Duplicates
+df.drop_duplicates(subset=['@id'], inplace=True)
