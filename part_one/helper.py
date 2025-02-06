3aa382b020135b007b54ce5e4811b27a4d3affda
Mehul Shetty <mehul@MacBook-18-Pro-Max-Deluxe-Edition.local>
2025-02-06 15:54:29 -0800

    graphed the execution time for knn brute force in python

--- part_one/helper.py
+++ part_one/helper.py
@@ -3,10 +3,8 @@
 import math
 
 def euclidean_distance(point_x, point_y) -> float:
-    x1: float = point_x['@lat']
-    y1: float = point_x['@lon']
-    x2: float = point_y['@lat']
-    y2: float = point_y['@lon']
+    x1, y1 = point_x['@lat'], point_x['@lon']
+    x2, y2 = point_y['@lat'], point_y['@lon']
 
     distance = math.sqrt(math.pow((x2 - x1), 2) + math.pow((y2 - y1), 2))
     return distance
