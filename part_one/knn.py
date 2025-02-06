9a1629ebb852626ca1a799df8fe6e50f1d017965
Mehul Shetty <mehul@MacBook-18-Pro-Max-Deluxe-Edition.local>
2025-02-06 15:09:17 -0800

    added brute force knn algo

--- part_one/knn.py
+++ part_one/knn.py
@@ -1,4 +1,16 @@
 ### knn.py --- Finds the k closest POIs to a target POI using linear search
 
 from helper import euclidean_distance
+import pandas as pd
 
+def knn_linear_search(dataset: pd.DataFrame, target_id: int, k: int):
+
+    POI = dataset.loc[dataset['@id'] == target_id].iloc[0]
+
+    dataset['@dist'] = dataset.apply(lambda row: euclidean_distance(row, POI), axis=1)
+
+    top_k = dataset.nsmallest(k+1, '@dist', keep='first')
+
+    result = top_k[1:][['@id', '@dist']].values.tolist()
+
+    return result
