207af1acdc6a62c00c82abf18e91239feaa9623d
Mehul Shetty <mehul@MacBook-18-Pro-Max-Deluxe-Edition.local>
2025-02-06 16:51:09 -0800

    added grid_index and kd_tree file skeleton

---
+++ part_one/grid_index.py
@@ -0,0 +1,7 @@
+### grid_index.py --- Builds a grid index which divides the space into uniform cells 
+# and assigns each point to a cell based on its coordinates
+
+import pandas as pd
+
+def build_grid_index(dataset: pd.DataFrame, cell_size: float):
+    return None
---
+++ part_one/kd_tree.py
@@ -0,0 +1,7 @@
+### kd_tree.py --- Creates a KD-Tree Indes which is a binary search tree that 
+# recursively partitions the space into half-spaces, adapting to the data distribution
+
+import pandas as pd
+
+def build_kd_tree(dataset: pd.DataFrame):
+    return None
