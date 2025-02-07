193d3874ca24162b59e2276e55a503c9c46bb32e
Mehul Shetty <mehul@MacBook-18-Pro-Max-Deluxe-Edition.local>
2025-02-06 16:44:19 -0800

    added code for range_query and it's graph plot

--- part_one/range_query.py
+++ part_one/range_query.py
@@ -1,4 +1,4 @@
-### knn.py --- Finds the k closest POIs to a target POI using linear search
+### range_query.py --- Finds all POIs within a specified distance r from a target POI using linear search
 
 from helper import euclidean_distance
 import pandas as pd
@@ -7,14 +7,14 @@ import matplotlib.pyplot as plt
 import time
 import random
 
-def knn_linear_search(dataset: pd.DataFrame, target_id: int, k: int):
+def range_query_linear_search(dataset: pd.DataFrame, target_id: int, r: float):
     """
-    Finds the k-nearest neighbors to a target POI using linear search.
+    Finds all POIs within r distance to a target POI using linear search.
 
     Parameters:
         dataset (pd.DataFrame): A DataFrame containing POI data with columns '@id', '@lat', '@lon', and 'name'.
         target_id (int): The ID of the target POI.
-        k (int): The number of nearest neighbors to find.
+        r (float): The distance within which to find the POIs.
 
     Returns:
         list: A list of the '@id' and '@dist' of the k-nearest neighbors.
@@ -24,53 +24,52 @@ def knn_linear_search(dataset: pd.DataFrame, target_id: int, k: int):
 
     dataset['@dist'] = dataset.apply(lambda row: euclidean_distance(row, POI), axis=1)
 
-    top_k = dataset.nsmallest(k+1, '@dist', keep='first')
-
-    result = top_k[1:][['@id', '@dist']].values.tolist()
+    result = dataset.loc[(dataset['@dist'] > 0) & (dataset['@dist'] <= r)].values.tolist()
 
     return result
 
-def calculate_execution_time(N_list: list, k_list: list, dataset: pd.DataFrame):
+def calculate_execution_time(N_list: list, r_list: list, dataset: pd.DataFrame):
     """
-    Calculates the execution times of the kNN query for different dataset sizes (N) and k values.
+    Calculates the execution times of the kNN query for different dataset sizes (N) and r values.
 
     Parameters:
         N_list : List of sizes for the dataset.
-        k_list : A list of k values to test.
+        r_list : A list of r values to test.
         dataset : The dataset for which we have to calculate execution times.
 
     Returns:
-        pd.DataFrame: A DataFrame containing the execution times for each combination of dataset size and k.
+        pd.DataFrame: A DataFrame containing the execution times for each combination of dataset size and r.
     """
 
-    results = {k: [] for k in k_list}
+    results = {r: [] for r in r_list}
 
     for n in N_list:
         mini_df = dataset[:min(n, len(dataset))].copy()
 
-        for k in k_list:
+        for r in r_list:
             target_id = random.choice(mini_df['@id'].values)
 
             start_time = time.time()
-            knn_linear_search(mini_df, target_id, k)
+            range_query_linear_search(mini_df, target_id, r)
             total_time = time.time() - start_time
 
-            results[k].append(total_time)
+            results[r].append(total_time)
 
     return results
 
 # Plot the Data
+
 df = pd.read_csv('../dataset/clean_nyc_dataset.csv')
 
-k_list = [1, 5, 10, 50, 100, 500]
+r_list = [0.01, 0.05, 0.1, 0.2, 0.5]
 N_list = [1000, 10000, 100000, 825171]
 
-plot_data = calculate_execution_time(N_list, k_list, df)
+plot_data = calculate_execution_time(N_list, r_list, df)
 
-for k, exec_time in plot_data.items():
-    plt.plot(N_list, exec_time, label=f'k = {k}', marker='o')
+for r, exec_time in plot_data.items():
+    plt.plot(N_list, exec_time, label=f'r = {r}', marker='o')
 
-plt.title('Execution Time vs Dataset Size (N) for Different k Values')
+plt.title('Execution Time vs Dataset Size (N) for Different r Values')
 plt.xlabel('Dataset Size (N)')
 plt.ylabel('Execution Time (seconds)')
 plt.legend()
